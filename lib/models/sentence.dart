1f0000aa7c5f8acd0b5826d0875c385e53b153f2
David Chopin <chopindavid@outlook.com>
2025-03-04 08:10:35 -0600

    Made it so that sentence exercises are cached - DC

--- lib/models/sentence.dart
+++ lib/models/sentence.dart
@@ -33,54 +33,58 @@ class Sentence extends Question<WordForm> {
   factory Sentence.fromJson(Map<String, dynamic> json) {
     final String? levelString = json['level'];
 
-    return Sentence._(
-      correctAnswer: WordForm.fromJson(json),
-      answerSynonyms: json['answer_synonyms']
-          .map<WordForm?>((e) {
-            try {
-              return WordForm.fromJson(e);
-            } catch (e) {
-              return null;
-            }
-          })
-          .whereType<WordForm>()
-          .toList(),
-      possibleAnswers: json['possible_answers']
-          .map<WordForm?>((e) {
-            try {
-              return WordForm.fromJson(e);
-            } catch (e) {
-              return null;
-            }
-          })
-          .whereType<WordForm>()
-          .toList(),
-      explanation: json['explanation'],
-      visualExplanation: json['visual_explanation'],
-      id: json.parseIntForKey('id'),
-      ru: json['ru'],
-      tatoebaKey: json.parseOptionalIntForKey('tatoeba_key'),
-      disabled: json.parseBoolForKey('disabled'),
-      level: StringExtension.isNullOrEmpty(levelString)
-          ? null
-          : Level.values.byName(levelString!),
-      word: Word.fromJson({
-        'id': json['word_id'],
-        'position': json['position'],
-        'bare': json['bare'],
-        'accented': json['accented'],
-        'derived_from_word_id': json['derived_from_word_id'],
-        'rank': json['rank'],
-        'disabled': json['word_disabled'],
-        'audio': json['audio'],
-        'usage_en': json['usage_en'],
-        'usage_de': json['usage_de'],
-        'number_value': json['number_value'],
-        'type': json['type'],
-        'level': json['word_level'],
-        'created_at': json['created_at']
-      }),
-    );
+    try {
+      return Sentence._(
+        correctAnswer: WordForm.fromJson(json),
+        answerSynonyms: json['answer_synonyms']
+            .map<WordForm?>((e) {
+              try {
+                return WordForm.fromJson(e);
+              } catch (e) {
+                return null;
+              }
+            })
+            .whereType<WordForm>()
+            .toList(),
+        possibleAnswers: json['possible_answers']
+            .map<WordForm?>((e) {
+              try {
+                return WordForm.fromJson(e);
+              } catch (e) {
+                return null;
+              }
+            })
+            .whereType<WordForm>()
+            .toList(),
+        explanation: json['explanation'],
+        visualExplanation: json['visual_explanation'],
+        id: json.parseIntForKey('id'),
+        ru: json['ru'],
+        tatoebaKey: json.parseOptionalIntForKey('tatoeba_key'),
+        disabled: json.parseBoolForKey('disabled'),
+        level: StringExtension.isNullOrEmpty(levelString)
+            ? null
+            : Level.values.byName(levelString!),
+        word: Word.fromJson({
+          'id': json['word_id'],
+          'position': json['position'],
+          'bare': json['bare'],
+          'accented': json['accented'],
+          'derived_from_word_id': json['derived_from_word_id'],
+          'rank': json['rank'],
+          'disabled': json['word_disabled'],
+          'audio': json['audio'],
+          'usage_en': json['usage_en'],
+          'usage_de': json['usage_de'],
+          'number_value': json['number_value'],
+          'type': json['type'],
+          'level': json['word_level'],
+          'created_at': json['created_at']
+        }),
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Map<String, dynamic> toJson() {
@@ -89,6 +93,7 @@ class Sentence extends Question<WordForm> {
       'word_form_position': correctAnswer.position,
       'form': correctAnswer.form,
       '_form_bare': correctAnswer.bare,
+      'correct_answer': correctAnswer.bare,
       'answer_synonyms': answerSynonyms.map((e) => e.toJson()).toList(),
       'possible_answers': possibleAnswers.map((e) => e.toJson()).toList(),
       'explanation': explanation,
